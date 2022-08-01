d3a4600c390c83a4e27937e5844ea83ae819aa83
sdk2316 <92987709+sdk2316@users.noreply.github.com>
2022-08-01 21:37:43 +0530

    Add files via upload

---
+++ ExcelPostMapping/src/test/java/com/durgesh/cog/ExcelPostMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durgesh.cog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelPostMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
